feef01f9b24c37bc74fb7c70c7b81d5c6b5503de
Joseph <joseph.a.gauthier@gmail.com>
2018-02-16 21:18:36 -0600

    Main initial commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,24 @@
+
+app.controller('MainController', function($scope, $http) {
+    $http.get('http://localhost:8080/getAccounts').
+        then(function(response) {
+            $scope.myData = response.data;
+        }, function(response) {
+        //Error function
+        $scope.content = "Something went wrong: " + response;
+    });    
+    
+    $scope.login = function() {
+        
+        for(var i = 0; i < $scope.myData.length; i++) { 
+            if ($scope.myData[i].value1 == $scope.name) {
+                $scope.content = ":D Now what";
+                
+                //console.log("HEY!");
+            }
+            else {
+                //console.log("lol");
+            }
+        }
+    }
+});
